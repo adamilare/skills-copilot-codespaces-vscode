ad688dc7f2c7b8c59469d37c8ce12f1c322523ec
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-07-31 17:31:48 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,21 +1,55 @@
-// create simple web server with express
-// 1. npm init -y
-// 2. npm install express
-// 3. create comments.js file
-// 4. node comments.js
-// 5. go to http://localhost:3000
+// Create web server
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { randomBytes } = require('crypto');
+const axios = require('axios');
+
 const app = express();
-const port = 3000;
+const PORT = 4001;
 
-// app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// Data
+const commentsByPostId = {};
+
+// Routes
+app.get('/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  res.send(commentsByPostId[id] || []);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-// 6. add new route /comments
-app.get('/comments', (req, res) => {
-    res.send('This is a comments route!');
+app.post('/posts/:id/comments', async (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  const comments = commentsByPostId[id] || [];
+  comments.push({ id: commentId, content, status: 'pending' });
+  commentsByPostId[id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: { id: commentId, content, postId: id, status: 'pending' },
+  });
+  res.status(201).send(comments);
 });
+
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+  if (type === 'CommentModerated') {
+    const { id, postId, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((c) => c.id === id);
+    comment.status = status;
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: { id, postId, status, content },
+    });
+  }
+  res.send({});
+});
+
+// Start server
+app.listen(PORT, () => console.log(`Comments server listening on port ${PORT}`));
