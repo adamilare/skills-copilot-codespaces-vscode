0568fea49bdb34e05bcf5bd6240dce4dfb2dc39a
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-07-31 17:19:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,21 @@
-// create web server
+// create simple web server with express
+// 1. npm init -y
+// 2. npm install express
+// 3. create comments.js file
+// 4. node comments.js
+// 5. go to http://localhost:3000
 const express = require('express');
 const app = express();
 const port = 3000;
 
-// import routes
-const comments = require('./routes/comments');
+// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-// middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// use routes
-app.use('/comments', comments);
-
-// start server
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// 6. add new route /comments
+app.get('/comments', (req, res) => {
+    res.send('This is a comments route!');
 });
